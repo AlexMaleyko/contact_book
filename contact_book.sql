1b902537f3239b4ef5cd85255b3f6a74a2295c71
Alexey Maleyko <alexeymaleyko@gmail.com>
2017-05-23 13:26:48 +0400

    initial commit

---
+++ contact_book.sql
@@ -0,0 +1,126 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: contact_book
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `attachment`
+--
+
+DROP TABLE IF EXISTS `attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attachment` (
+  `attachment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `file_path` varchar(260) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `upload_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `comment` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deletion_date` timestamp NULL DEFAULT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`attachment_id`),
+  KEY `fk_attachment_contact` (`contact_id`),
+  CONSTRAINT `fk_attachment_contact` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attachment`
+--
+
+LOCK TABLES `attachment` WRITE;
+/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact` (
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patronymic` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birth` date DEFAULT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `citizenship` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `marital_status` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `job` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `street` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deletion_date` timestamp NULL DEFAULT NULL,
+  `profile_picture` varchar(260) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=215 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phone_number`
+--
+
+DROP TABLE IF EXISTS `phone_number`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phone_number` (
+  `number_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `country_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `operator_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `number` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `comment` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deletion_date` timestamp NULL DEFAULT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`number_id`),
+  KEY `fk_phone_number_contact` (`contact_id`),
+  CONSTRAINT `fk_phone_number_contact` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phone_number`
+--
+
+LOCK TABLES `phone_number` WRITE;
+/*!40000 ALTER TABLE `phone_number` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phone_number` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-23 13:24:57
---
+++ contactsbook/moduleB/src/main/webapp/resources/js/index.js
@@ -0,0 +1,132 @@
+
+function checkAll(mainCheckbox){
+    var checkboxes=document.getElementsByName("checkBoxGroup");
+    for (var i=0; i<checkboxes.length;i++) {
+        checkboxes[i].checked=mainCheckbox.checked;
+    }
+}
+
+var checkedBoxNumber = 0;
+function manageUpdateDeleteButtons(checkBox){
+    if(checkBox.id =="mainCheckBox" && checkBox.checked==true){
+        checkedBoxNumber=document.getElementsByName("checkBoxGroup").length;
+    }
+    if(checkBox.id =="mainCheckBox" && checkBox.checked==false){
+        checkedBoxNumber=0;
+    }
+    if(checkBox.id !="mainCheckBox") {
+        if (checkBox.checked) {
+            checkedBoxNumber++;
+        }
+        else {
+            checkedBoxNumber = checkedBoxNumber - 1;
+            document.getElementById("mainCheckBox").checked=false;// SWITCHES OFF THE MAIN CHECKBOX when one of checkboxes is unchecked
+        }
+    }
+        if (checkedBoxNumber !== 1) {
+            document.getElementById("updateContact").disabled = true;
+        }
+        else {
+            document.getElementById("updateContact").disabled = false;
+        }
+        if(checkedBoxNumber==0){
+            document.getElementById("deleteContact").disabled = true;
+        }
+        else{
+            document.getElementById("deleteContact").disabled = false;
+        }
+}
+
+
+function search() {
+    var input, filter, table,tbody, tr, a, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("contactTable");
+    tbody=table.getElementsByTagName("tbody")[0];
+    tr = tbody.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        a = tr[i].getElementsByTagName("a")[0];
+        if (a) {
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('pattern').addEventListener('change', function(){
+        var select = document.getElementById('pattern');
+        document.getElementById('mailText').value = select.options[select.selectedIndex].value;
+        document.getElementById('hiddenPattern').value = select.options[select.selectedIndex].getAttribute('name');
+        if(select.options[select.selectedIndex].getAttribute('name') != ""){
+            document.getElementById('mailText').disabled = true;
+        }
+        else{
+            document.getElementById('mailText').disabled = false;
+        }
+    });
+});
+function newPage (event) {
+    document.getElementById('clickedPage').value
+        = event.target.name;
+    document.getElementById('paginationForm').submit();
+}
+document.addEventListener('DOMContentLoaded', function() {
+    "use strict";
+    var pageTotal = document.getElementById('pageTotal').value;
+    var currentPage = document.getElementById('currentPage').value;
+    var clientLimit = document.getElementById('clientLimit').value;
+    var pagination = document.getElementById('pagination');
+    var button;
+
+    if(pageTotal == 0){
+        pagination.innerHTML = "Данные отсутствуют";
+    }
+    else {
+        for (var i = 0; i < pageTotal; i++) {
+            button = document.createElement('button');
+            button.setAttribute('type', 'submit');
+            button.setAttribute('name', i);
+            if (i == currentPage) {
+                button.setAttribute('class', 'active');
+            }
+            else {
+                button.setAttribute('class', 'paginationBtn');
+            }
+            button.value = i + 1;
+            button.innerHTML = i + 1;
+            pagination.appendChild(button);
+        }
+    }
+    var paginationButtons = document.getElementsByClassName('paginationBtn');
+    for(var i = 0; i < paginationButtons.length; i++){
+        paginationButtons[i].addEventListener('click', newPage, false);
+    }
+    document.getElementById('pageNum').value = clientLimit;
+    document.getElementById('pageNum').addEventListener('change', function () {
+        document.getElementById('clientLimit').value
+            = document.getElementsByName('pageNum')[0].value;
+        document.getElementById('paginationForm').submit();
+    })
+});
+document.addEventListener('DOMContentLoaded', function(){
+    if(document.getElementById("isSearchPage").value == 1){
+        document.getElementById("backToList").style.display = "block";
+    }
+});
+/*
+function addCheckBoxListener(){
+    alert("dfd");
+    var checkboxes=document.getElementsByName("checkBoxGroup");
+    for (var i=0; i<checkboxes.length;i){
+        checkboxes[i].addEventListener("change",manageUpdateButton);
+    }
+}
+
+document.onload = addCheckBoxListener();
+*/
